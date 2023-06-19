568778909c11206b51dbb9cd4ac9e7af2f615ea4
rzzkv <rzzkv@yandex.ru>
2023-06-19 07:44:18 +0500

    s

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -3,11 +3,15 @@ package school.mjc.stage0.loops.finalTask;
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
-            // Loop through each column in the row
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+            for (int j = 1; j <= cathetusLength - i; j++) {
+                System.out.print(" ");
             }
-            // Move to the next line after each row is printed
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+                if (k < i) {
+                    System.out.print("");
+                }
+            }
             System.out.println();
         }
     }
